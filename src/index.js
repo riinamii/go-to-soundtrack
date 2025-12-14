77d78127cf290507a8899d973d2640e44336f01c
riinamii <179370603+riinamii@users.noreply.github.com>
2025-12-14 21:01:59 +0200

    Add modal window functionality for lyrics

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "flex";
+}
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
